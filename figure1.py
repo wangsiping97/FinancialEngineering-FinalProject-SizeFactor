d9d0fb625fb45771362cb42e52882a802230785b
wangsiping97 <wangsiping1997@gmail.com>
2019-01-17 23:57:12 +0800

    Create figure1.py

---
+++ figure1.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import scipy.io as sio
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = sio.loadmat('CaseDataAll.mat')
+market_data = pd.read_csv('market.csv')
+eom = data["a_EOM"]
+mon = data["Mon_Yield"]
+size = data["Mon_SizeAll"]
+market_return = market_data['Mkt_Rf']
+risk_free = market_data['rf']
+
+for x in eom:
+    x[0] = int(x[0] / 100)  # change the month in eom
+
+size = np.array(size)
+ave_size = []
+skews = []
+kurts = []
+for i in range(60, np.array(size).shape[0]):
+    a = pd.Series(size[i])
+    ave_size.append(a.mean())
+    skews.append(a.skew())
+    kurts.append(a.kurt())
+
+df1 = pd.DataFrame({'month': np.arange(0, np.array(size).shape[0] - 60), 'ave_size': ave_size})
+
+# Draw Plot
+plt.figure(figsize=(16, 10), dpi=80)
+plt.plot('month', 'ave_size', data=df1, color='tab:red')
+
+# Decoration
+plt.ylim(0, 3000000)
+xtick_location = df1.index.tolist()[::12]
+xtick_labels = [(x // 12) + 2005 for x in df1.month.tolist()[::12]]
+plt.xticks(ticks=xtick_location, labels=xtick_labels, rotation=0, fontsize=12, horizontalalignment='center',
+           alpha=.7)
+plt.yticks(fontsize=12, alpha=.7)
+plt.grid(axis='both', alpha=.3)
+
+# Remove borders
+plt.gca().spines["top"].set_alpha(0.0)
+plt.gca().spines["bottom"].set_alpha(0.3)
+plt.gca().spines["right"].set_alpha(0.0)
+plt.gca().spines["left"].set_alpha(0.3)
+plt.gca().set(xlabel='Year', ylabel='Monthly Average of Size')
+plt.savefig('figure1.png')
